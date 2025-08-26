16b8d9041d02783243cb768c41cac7270da1aa37
xcaio <caiosantos1703@gmail.com>
2025-08-25 21:20:57 -0300

    update: home screen in App.jsx

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,10 +1,11 @@
 import Header from "./components/Header";
-import Item from "./components/Item";
+import Home from "./pages/Home";
+
 function App() {
   return (
     <>
       <Header />
-      
+      <Home/>
     </>
   );
 }
